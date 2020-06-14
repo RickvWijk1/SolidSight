0091c691b228b4d84c4fea5fc45037786f5e9662
RickvWijk <404350@student.fontys.nl>
2020-06-14 17:02:04 +0200

    Made the search bar work
    The search bar can now be used to filter

---
+++ searchbar.js
@@ -0,0 +1,26 @@
+//this function filters categories (using the search bar)
+function searchFunction() {
+    //initializing variables
+    var input, filter, allcontainer, gridItem, h4, i;
+    //declaring variables
+    input = document.getElementById('myInput');
+    //set the input of the search bar to capital letters 
+    filter = input.value.toUpperCase();
+    allcontainer = document.getElementById('allcontainer');
+    gridItem = allcontainer.getElementsByClassName('location-image');
+
+    //keep running loop if 'i' is less than the grid item's length
+    for (i = 0; i < gridItem.length; i++) {
+        h4 = gridItem[i].getElementsByTagName('h4')[0];
+
+        //the text in h4 is set to uppercase in order too match the input value of filter (a.k.a. the search bar) which is also set to uppercase
+        //if the h4 text contains the value that is in the search bar... else...
+        if (h4.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            //item is displayed
+            gridItem[i].style.display = "";
+        } else {
+            //item is not displayed
+            gridItem[i].style.display = 'none';
+        }
+    }
+}
